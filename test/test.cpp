bc9f093b424ddc62d3f7fce0cd2cefc1942b8fdb
Your Name <you@example.com>
2021-03-16 16:41:06 +0900

    add test

--- test/test.cpp
+++ test/test.cpp
@@ -13,6 +13,23 @@ TEST(push, push_r32) {
 }
 */
 
+/*
+TEST(mov, mov_rm32_imm32) {
+   mov    %esp,%edx
+   mov    $0x3,%eax
+   push   %eax
+   mov    (%edx),%ecx
+   pop    %eax
+*/
+
+TEST(mov, mov) {
+    uint8_t asmb[15] = {0x40, 0x01, 0xcb, 0xc3};
+    Ghyper *ghv = new Ghyper();
+    ghv->hv(asmb);
+
+    delete ghv;
+}
+
 TEST(mov, mov_rm32_imm32) {
     uint8_t asmb[15] = {0x89, 0xe2, 0xb8, 0x03, 0x00, 0x00, 0x00, 0x50, 0x8b, 0x0a, 0x58, 0xc3};
     Ghyper *ghv = new Ghyper();
